cab9370329abee941f5397d4e278a7d59cb522b5
YewonKimMe <kimyewon030428@gmial.com>
2024-08-26 18:15:38 +0900

    axios auth setting

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
-        'Accept': 'application/json'
+        'Accept': 'application/json',
+        'Authorization': import.meta.env.VITE_SECURITY_API_KEY,
     },
     withCredentials: true,
 });
