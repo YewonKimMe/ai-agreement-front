5148a53109ea9372786fd6407ed6d270e05cba87
YewonKimMe <kimyewon030428@gmial.com>
2024-08-30 08:04:50 +0900

    Authorization 제거

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,6 @@ const instance = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
         'Accept': 'application/json',
-        'Authorization': import.meta.env.VITE_SECURITY_API_KEY,
     },
     withCredentials: true,
 });
